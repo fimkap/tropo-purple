8bda7eefc6d6eac8db564f430a7cabdea7eb4701
fimkap <fimkap@gmail.com>
2012-07-23 20:28:25 +0300

    Add DB

--- tropocallback.py
+++ tropocallback.py
@@ -2,8 +2,16 @@ from tropo import Tropo
 import logging
 import json
 import webapp2
-#from google.appengine.ext import webapp
-#from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+
+class PurpleUser(db.Model):
+    username = db.StringProperty()
+    phonosid = db.StringProperty()
+    presence = db.IntegerProperty()
+
+def userbook_key(username):
+  return db.Key.from_path('Userbook', username)
+    
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -19,10 +27,32 @@ class MainPage(webapp2.RequestHandler):
         json = tropo.RenderJson()
         logging.info ("Sending json back to Tropo")
         self.response.out.write(json)
-    
+
+
+class UserBook(webapp2.RequestHandler):
+    def post(self):
+        username = self.request.params.get("name")
+        purpleuser = PurpleUser(parent=userbook_key(username))
+        purpleuser.username = username;
+        sid = self.request.params.get("sid")
+        purpleuser.phonosid = sid;
+        purpleuser.presence = 1
+        purpleuser.put()
+        # TODO don't return signed in user's own name
+        q = db.GqlQuery("SELECT * FROM PurpleUser ")
+        users = q.fetch(limit=None)
+        usersDict = {}
+        for user in users:
+            usersDict[user.username] = { 'name' : user.username, 'sid' : user.phonosid, 'presence' : user.presence }
+        self.response.out.write(json.dumps(usersDict))
+
+    def get(self):
+        return self.post()
+
 
 app = webapp2.WSGIApplication(
-                                     [('/', MainPage)],
+                                     [('/', MainPage),
+                                     ('/sign', UserBook)],
                                      debug=True)
 
 #def main():

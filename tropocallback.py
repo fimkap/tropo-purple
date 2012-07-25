0ef216e4d359319330dbb2f9a6eba451b223d10c
fimkap <fimkap@gmail.com>
2012-07-25 19:14:06 +0300

    Pass params (purple pal) with the call

--- tropocallback.py
+++ tropocallback.py
@@ -1,4 +1,4 @@
-from tropo import Tropo
+from tropo import Tropo, Session
 import logging
 import json
 import webapp2
@@ -15,15 +15,28 @@ def userbook_key(username):
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        username = self.request.params.get("name")
-        logging.info("User name is %s" % username)
+        logging.info("Shouldn't get here with POST")
+        #username = self.request.params.get("name")
+        #logging.info("User name is %s" % username)
         #self.response.headers['Content-Type'] = 'text/plain'
-        #self.response.out.write('Hello, webapp World!')
-        self.response.out.write(json.dumps(username))
+        #self.response.out.write(self.request.params)
+        #self.response.out.write(json.dumps(username))
 
     def post (self):
+        s = Session(self.request.body)
+        #logging.info("session: %s " % self.request.body)
+        callerID = s.fromaddress['id']
+        pal = s.headers['x-username']
+        
+        logging.info ("Caller ID: %s " % callerID)
+        logging.info ("Pal : %s " % pal)
+        # Find sid for this pal
+        users = PurpleUser.all()
+        users.filter("username == ", pal)
+
         tropo = Tropo()
-        tropo.transfer("sip:d6561a48-b069-4f31-9166-d0d32f0e8fe4@phono3-ext.voxeolabs.net")
+        #tropo.transfer("sip:d6561a48-b069-4f31-9166-d0d32f0e8fe4@phono3-ext.voxeolabs.net")
+        tropo.say("Hello Everybody")
         json = tropo.RenderJson()
         logging.info ("Sending json back to Tropo")
         self.response.out.write(json)
@@ -38,12 +51,16 @@ class UserBook(webapp2.RequestHandler):
         purpleuser.phonosid = sid;
         purpleuser.presence = 1
         purpleuser.put()
-        # TODO don't return signed in user's own name
-        q = db.GqlQuery("SELECT * FROM PurpleUser ")
-        users = q.fetch(limit=None)
-        usersDict = {}
-        for user in users:
-            usersDict[user.username] = { 'name' : user.username, 'sid' : user.phonosid, 'presence' : user.presence }
+        # TODO hide JSON conversion in model class
+        users = PurpleUser.all()
+        users.filter("username != ", username)
+        usersDict = dict((user.username, {'sid' : user.phonosid, 'presence' : user.presence }) for user in users)
+
+        #q = db.GqlQuery("SELECT * FROM PurpleUser ")
+        #users = q.fetch(limit=None)
+        #usersDict = {}
+        #for user in users:
+        #    usersDict[user.username] = { 'name' : user.username, 'sid' : user.phonosid, 'presence' : user.presence }
         self.response.out.write(json.dumps(usersDict))
 
     def get(self):
